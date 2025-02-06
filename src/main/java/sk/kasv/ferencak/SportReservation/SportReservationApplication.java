12be34762f4fe65e25e361fc066095ff3ff0d7ba
Jakub Ferenčák <jakub.ferencak@kosickaakademia.sk>
2025-02-06 13:38:17 +0100

    Prvy commit

---
+++ src/main/java/sk/kasv/ferencak/SportReservation/SportReservationApplication.java
@@ -0,0 +1,13 @@
+package sk.kasv.ferencak.SportReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/kasv/ferencak/SportReservation/SportReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.kasv.ferencak.SportReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
